31d3090a3ad85163d1c8c51e527f1748701f1e61
angiecodes87 <angelcoder87@gmail.com>
2023-05-09 01:17:32 +0300

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file with the given content
+ * @filename: the name of the file to create
+ * @text_content: the content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+	return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+	return (-1);
+
+	if (text_content)
+	{
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1)
+	{
+	close(fd);
+	return (-1);
+	}
+	}
+
+	close(fd);
+	return (1);
+}
+
