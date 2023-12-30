488f1d2ed6846039005156dc9df1fb39f1987cf7
Angiecodes <angelcoder87@gmail.com>
2023-12-30 11:55:35 +0300

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,45 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using Linear search
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: The value to search for
+ * linear_search - Searches for a value in an array of integers using
+ * linear search
+ * @array: Pointer to the array of integers
+ * @size: Size of the array
+ * @value: Value to search for
  *
- * Return: The first index where the value is located,
- *         or -1 if the value is not present or if the array is NULL.
+ * Return: Index of the value if found, otherwise -1
  */
 int linear_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
+	size_t index;
 
-	for (size_t i = 0; i < size; i++)
+	if (array == NULL)
+	return (-1);
+
+	for (index = 0; index < size; index++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		return (i);
+	printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+	if (array[index] == value)
+	return (index);
 	}
 
 	return (-1);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-	int array[] = {10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
-	size_t size = sizeof(array) / sizeof(array[0]);
-
-	printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-	printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-	printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-
-	return (EXIT_SUCCESS);
 }
 
